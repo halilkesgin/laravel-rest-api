f4c493f4b64acd749add9cea0ea25f63e0ba2e0f
Halil <halillkesgin@gmail.com>
2024-02-20 00:47:30 +0300

    auth done

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,8 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            "password" => '$2y$12$bTacvCL1hZgM7bWSQWxvlucTWCEqFpZ88piJChj6ZrLqQHI/cgaPO',
+            // 'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
